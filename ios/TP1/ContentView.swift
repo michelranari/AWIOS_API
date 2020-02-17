aaca36ba649186c5c9a5d6640e3e2b10f4ab2d95
bahroun <r.bahroun@gmail.com>
2020-02-17 17:02:52 +0100

    formulaire ok

--- ios/TP1/ContentView.swift
+++ ios/TP1/ContentView.swift
@@ -9,27 +9,35 @@
 import SwiftUI
 
 struct ContentView: View  {
-    @State var persons = Persons(persons: [
+    @State var persons : [Person] =  [
         Person(name:"nom",firstName:"prenom",department: "department", profession: "profession"),
         Person(name:"Menouer",firstName:"Michel",department: "IG4", profession: "student"),
         Person(name:"Mas",firstName:"Amjad",department: "IG4", profession: "student"),
         Person(name:"Ranarimahefa",firstName:"Rayan",department: "IG4", profession: "student"),
         Person(name:"Bahroun",firstName:"Lucas",department: "IG4", profession: "student"),
-    ])
+    ]
     var body: some View {
-            NavigationView{
-                VStack {
-                    NavigationLink(destination : PersonForm(persons: persons)){
-                        Text("Ajouter personne")
-                    }
-                    List{
-                        ForEach(0..<persons.count, id : \.self){ index in
-                            NavigationLink(destination: PersonDetail(person: persons[index])){
-                                PersonRow( person: persons[index])
-                            }
+        
+        NavigationView{
+            VStack {
+                List{
+                    Section(header : Text("Ajout")){
+                        NavigationLink(destination : PersonForm(persons : $persons )){
+                            Text("Ajouter personne")
                         }
                     }
-                }
+                    Section(header : Text("Liste étudiants")){
+                        ForEach(0..<persons.count, id : \.self){ index in
+                            NavigationLink(destination: PersonDetail(person: self.$persons[index])){
+                                PersonRow( person: self.$persons[index])
+                            }
+                            
+                        }
+                    }
+                }.font(.headline)
+            }
+            .navigationBarTitle(Text("Liste Etudiants"))
+            .navigationBarItems(trailing: EditButton())
         }
     }
 }
--- ios/TP1/PersonDetail.swift
+++ ios/TP1/PersonDetail.swift
@@ -7,7 +7,6 @@
 //
 
 import SwiftUI
-import Combine
 
 
 struct PersonDetail: View {
