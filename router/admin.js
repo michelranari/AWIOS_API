21d90d5af4a6a9a008fb92c4eea0084c1aedc1a7
Michel Rana <michel.ranari@gmail.com>
2020-04-02 02:25:08 +0200

    test apidoc deploy

--- router/admin.js
+++ router/admin.js
@@ -370,15 +370,24 @@ router.get('/apidoc', (req, res) => {
 
   console.log(process.cwd())
 
-  fs.readFile('../apidoc/index.html', function(err, data) {
+  fs.readdir('./', (err, files) => {
     if(err){
-      console.log(err)
-      return res.status(500).send(err);
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
+        console.log(err)
+      }
+    files.forEach(file => {
+      console.log(file);
+    });
   });
+
+  // fs.readFile('../apidoc/index.html', function(err, data) {
+  //   if(err){
+  //     console.log(err)
+  //     return res.status(500).send(err);
+  //   }
+  //   res.writeHead(200, {'Content-Type': 'text/html'});
+  //   res.write(data);
+  //   res.end();
+  // });
 })
 
 
